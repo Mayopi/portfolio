a2787ee54584e735a916f63d243a22a5bb7e0605
Mayopi <cyade007@gmail.com>
2023-07-09 23:58:44 +0700

    new guestbook page

--- src/pages/guestbook/index.tsx
+++ src/pages/guestbook/index.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Head from "next/head";
 import { FaBookReader } from "react-icons/fa";
 import { BiUpvote, BiSolidUpvote } from "react-icons/bi";
+import { MdArticle } from "react-icons/md";
 
 const GuestBookItem: React.FC<{ user: string; date: string; title: string; content: string }> = ({ user, title, content, date }): React.ReactNode => {
   return (
@@ -22,19 +23,21 @@ const GuestBookItem: React.FC<{ user: string; date: string; title: string; conte
         </h3>
       </div>
 
-      <div className="body p-3 border border-opacity-10 w-full rounded-lg bg-base-200 mr-5">
-        <h1 className="mb-2 font-semibold text-lg">{title}</h1>
-        <p>{content}</p>
+      <Link href={`/guestbook/${title.split(" ").join("-").toLowerCase()}`} className="cursor-pointer">
+        <div className="body p-3 shadow w-full rounded-lg bg-base-200 mr-5">
+          <h1 className="mb-2 font-semibold text-lg">{title}</h1>
+          <p>{content}</p>
 
-        <div className="footer mt-3 flex gap-5">
-          <Link href={`/guestbook/${title.split(" ").join("-").toLowerCase()}`}>
-            <p className="hover:link link-info">View Guestbook</p>
-          </Link>
-          <button className="text-lg">
-            <BiUpvote />
-          </button>
+          <div className="footer mt-3 flex gap-5">
+            <Link href={`/guestbook/${user.split(" ").join("-").toLowerCase()}`}>
+              <p className="hover:link link-info">View Guestbooks</p>
+            </Link>
+            <button className="text-lg">
+              <BiUpvote />
+            </button>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
@@ -50,10 +53,16 @@ const GuestBook: React.FC = (): React.ReactNode => {
         <Navbar />
 
         <header className="w-full flex justify-center items-center">
-          <h1 className="lg:text-2xl text-xl font-semibold uppercase tracking-wide flex justify-center items-center gap-2">
+          <h1 className="lg:text-2xl text-xl font-semibold mx-auto uppercase tracking-wide flex justify-center items-center gap-2">
             <FaBookReader className="inline" />
             Guestbooks
           </h1>
+
+          <Link href={"/guestbook/new"}>
+            <button className="btn btn-primary btn-outline text-2xl p-2 aspect-square">
+              <MdArticle />
+            </button>
+          </Link>
         </header>
 
         <section className="guestbook my-10 lg:mx-56 mx-10 flex flex-wrap gap-6">
@@ -67,7 +76,7 @@ const GuestBook: React.FC = (): React.ReactNode => {
           <GuestBookItem
             user="Ocha"
             date="Last Week"
-            title="Book Title"
+            title="hello this is test mothafaking title"
             content="
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur at facere praesentium? Expedita, tempora. Modi vel dolore accusantium magni inventore repellendus temporibus minima placeat necessitatibus, reprehenderit veritatis? Beatae, perspiciatis ipsum."
           />
---
+++ src/pages/guestbook/new/index.tsx
@@ -0,0 +1,101 @@
+import { FC, ReactNode, useState } from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Head from "next/head";
+import { MdArticle, MdOutlineFormatListBulleted } from "react-icons/md";
+import { BiHeading, BiBold, BiItalic } from "react-icons/bi";
+import { GoQuote } from "react-icons/go";
+import { BsCode, BsListOl, BsListTask } from "react-icons/bs";
+import { FiLink } from "react-icons/fi";
+
+const NewGuestBook: FC = (): ReactNode => {
+  const [content, setContent] = useState("");
+
+  const markdownSyntax = (startTag: string, endTag: string) => {
+    const textarea = document.getElementById("guestbook") as HTMLTextAreaElement;
+
+    const { selectionStart, selectionEnd } = textarea;
+    const selectedText: string = textarea.value.substring(selectionStart, selectionEnd);
+    const newText: string = `${startTag}${selectedText}${endTag}`;
+
+    const updatedContent = textarea.value.substring(0, selectionStart) + newText + textarea.value.substring(selectionEnd);
+
+    setContent(updatedContent);
+
+    const newCursorStartPosition: number = selectionStart + startTag.length;
+    const newCursorEndPosition: number = selectionStart + startTag.length + selectedText.length;
+    setTimeout(() => {
+      textarea.focus();
+      textarea.setSelectionRange(newCursorStartPosition, newCursorEndPosition);
+    }, 0);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Portfolio - New Guestbook</title>
+      </Head>
+      <main className="mx-5 pb-24 lg:mx-16">
+        <Navbar />
+
+        <section className="w-full">
+          <h1 className="font-semibold text-lg lg:text-2xl uppercase flex justify-center items-center gap-2 tracking-wide text-center">
+            <MdArticle className="inline" /> Create new Guestbook
+          </h1>
+          <form className="w-full mt-10">
+            <div className="w-full bg-base-200 min-h-[60px] rounded-t flex justify-between">
+              <div className="h-[60px] flex items-end w-1/2 px-5">
+                <div className="tabs">
+                  <a className="tab tab-bordered tab-active">Write</a>
+                  <a className="tab tab-bordered">Preview</a>
+                </div>
+              </div>
+
+              <div className="editors w-1/2 flex gap-5 justify-end px-10">
+                <div className="typography flex gap-2 items-center justify-center">
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Heading" onClick={() => markdownSyntax("# ", "")}>
+                    <BiHeading />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Bold" onClick={() => markdownSyntax("**", "**")}>
+                    <BiBold />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Italic" onClick={() => markdownSyntax("_", "_")}>
+                    <BiItalic />
+                  </button>
+                </div>
+
+                <div className="blocks flex gap-2 items-center justify-center">
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Quote Block" onClick={() => markdownSyntax("> ", "")}>
+                    <GoQuote />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Code Block" onClick={() => markdownSyntax("`", "`")}>
+                    <BsCode />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Hyperlink" onClick={() => markdownSyntax("[", "](url)")}>
+                    <FiLink />
+                  </button>
+                </div>
+
+                <div className="lists flex gap-2 items-center justify-center">
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Bullet List" onClick={() => markdownSyntax("- ", "")}>
+                    <MdOutlineFormatListBulleted />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Number List" onClick={() => markdownSyntax("1. ", "")}>
+                    <BsListOl />
+                  </button>
+                  <button type="button" className="text-xl hover:text-primary tooltip tooltip-info" data-tip="Task List" onClick={() => markdownSyntax("- [] ", "")}>
+                    <BsListTask />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <textarea name="content" id="guestbook" rows={30} className="bg-transparent rounded-b w-full h-[400px] border border-slate-700 p-3" placeholder="Type Here" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default NewGuestBook;
